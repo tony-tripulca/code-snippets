85256f9b7d6b6d95e78383f21ddc1cf31f01f19d
tony <tony.tripulca@gmail.com>
2020-04-12 23:32:07 +0800

    Snippet for base64 to blob

---
+++ b64-to-blob/readme.md
@@ -0,0 +1,3 @@
+# Base 64 to Blob
+
+Convert a base64 string in a Blob according to the data and contentType.
---
+++ b64-to-blob/script.js
@@ -0,0 +1,50 @@
+// Assuming the we are using https://fengyuanchen.github.io/cropperjs/
+var ImageURL = cropper.getCroppedCanvas().toDataURL();
+
+// Split the base64 string in data and contentType
+var block = ImageURL.split(";");
+
+// Get the content type of the image
+var contentType = block[0].split(":")[1];// In this case "image/gif"
+
+// get the real base64 content of the file
+var realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
+
+// Convert it to a blob to upload
+var blob = b64toBlob(realData, contentType);
+
+// Append the file with "image" as parameter name
+form_data.append("image", blob);
+
+/**
+ * Convert a base64 string in a Blob according to the data and contentType.
+ * 
+ * @param b64Data {String} Pure base64 string without contentType
+ * @param contentType {String} the content type of the file i.e (image/jpeg - image/png - text/plain)
+ * @param sliceSize {Int} SliceSize to process the byteCharacters
+ * @see http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+ * @return Blob
+ */
+function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+  var blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
